b2a9d501a5357bf704485f2fb4cd7de9de2e860e
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-25 23:34:45 +0000

    feat(MacAddressMetadata): POJO for mac address metadata

---
+++ app/src/main/java/org/example/model/MacAddressMetadata.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MacAddressMetadata {
+    @JsonProperty("mac_address")
+    private String macAddress;
+}
