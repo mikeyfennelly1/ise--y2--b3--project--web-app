c5be2bff16296b11ec547b06a4079779184493c3
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-02-23 11:48:12 +0000

    feat: Device

---
+++ app/src/main/java/org/example/model/source/device/Device.java
@@ -0,0 +1,24 @@
+package org.example.model.source.device;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "devices")
+public class Device {
+
+    @Id
+    @JsonProperty("mac_address")
+    private String macAddress;
+
+    @JsonProperty("device_name")
+    private String deviceName;
+
+    private String owner;
+}
