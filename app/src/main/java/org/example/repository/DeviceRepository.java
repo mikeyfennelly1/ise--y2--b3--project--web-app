d98d8120f53dde5807f03ea6effb29a934ac002e
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-02-23 11:47:53 +0000

    feat: DeviceRepository

---
+++ app/src/main/java/org/example/repository/DeviceRepository.java
@@ -0,0 +1,9 @@
+package org.example.repository;
+
+import org.example.model.source.device.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, String> {
+}
