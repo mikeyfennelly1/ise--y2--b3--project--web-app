97ed2733cee2e57fe53642838874d088af6ef555
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-02-28 19:22:00 +0000

    refactor: remove concrete POJOs for Devices, MacAddresses et al

--- app/src/main/java/org/example/consumer/model/device/DeviceType.java
+++
@@ -1,5 +0,0 @@
-package org.example.consumer.model.device;
-
-public enum DeviceType {
-    LAPTOP
-}
--- app/src/main/java/org/example/consumer/repository/DeviceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.consumer.repository;
-
-import org.example.consumer.model.device.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeviceRepository extends JpaRepository<Device, String> {
-}
